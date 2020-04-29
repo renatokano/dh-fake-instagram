e55b29e1fed27cd2098b0d2204dd77d8d5665797
Daniel <contatodanielniviera@outlook.com>
2020-04-29 17:23:24 -0300

    Add: Configurando o controller, organizando as rotas  e inserindo dados no banco com o sequelize

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   username: 'root',
-  password: 'docker',
+  password: '1234',
   database: 'fake-instagram',
   host: '127.0.0.1',
   dialect: 'mysql'
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
-  res.render("auth/login", { title: "Express" });
-});
+const indexController = require('../controller/indexController')
 
-router.get("/login", function (req, res, next) {
-  res.render("auth/login", { title: "Express" });
-});
+router.get("/", indexController.index)
 
-router.get("/registro", function (req, res, next) {
-  res.render("auth/register", { title: "Express" });
-});
 
-router.get("/home", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router.get("/login", indexController.login)
+
+
+router.get("/registro", indexController.create)
+router.post('/registro', indexController.store)
+
+
+router.get("/home", indexController.home)
+
+
 
 module.exports = router;
