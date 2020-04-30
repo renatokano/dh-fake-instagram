3328081e6de2930af9bd3be6e7fde3cc3d45bfc8
renatokano <renatokano16@gmail.com>
2020-04-30 20:11:49 -0300

    Update: Sign up logic

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,30 +1,54 @@
-const Sequelize = require('sequelize')
-const config = require('../config/database') 
-const db = new Sequelize(config)
+const Sequelize = require('sequelize');
+const config = require('../config/database');
+const db = new Sequelize(config);
+const bcrypt = require('bcrypt');
+const SALT = 10;
 
 let controller = {
-  home:(_req, res) => res.render("index", { title: "Express" }),
+  home: (req, res) => {
+    const {name, email, username} = req.session.user; 
+    console.log(name, email, username);
+    res.render("index", { 
+      title: "Express",
+      name,
+      username,
+      email
+    });
+  },
 
   create:  (_req, res) => res.render("auth/register", { title: "Express" }),
 
   store: (req, res) =>{
-      let {email, name, username, password} = req.body;
+    let {email, name, username, password} = req.body;
+    bcrypt.hash(password, SALT).then(hashPassword => {
       db.query("INSERT INTO users (name, email, username, password, created_at, updated_at) VALUES(:name, :email, :username, :password, :created_at, :updated_at)", { 
         type: Sequelize.QueryTypes.INSERT, 
         replacements: {
           name, 
           email, 
           username, 
-          password, 
+          password: hashPassword, 
           created_at: new Date(), 
           updated_at: new Date()
         } 
       }).then((user) =>{
-          !user ? res.render("auth/register", {msg: "Erro ao cadastrar usuário"}) : res.redirect("/home");
+          if(!user){
+            res.render("auth/register", {
+              msg: "Erro ao cadastrar usuário"
+            });
+          } else {
+            req.session.user = {
+              name,
+              email,
+              username
+            };
+            res.redirect("/home");
+          }
       }).catch(err => {
         res.render("auth/register", {msg: "Erro ao cadastrar usuário"});
         console.log(err);
-      });
+      })
+    });
   }
 }
 
