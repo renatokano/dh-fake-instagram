470bd7849f387266dd028ec5baa57f37c0f5ff07
renatokano <renatokano16@gmail.com>
2020-04-30 16:06:30 -0300

    Change: database configuration

--- config/database.js
+++ config/database.js
@@ -1,9 +1,7 @@
-let config = {
+module.exports = {
   username: 'root', 
   password: 'docker',
   database: 'fake_instagram',
   host: '127.0.0.1',
   dialect: 'mysql'
 }
-
-module.exports = config;
