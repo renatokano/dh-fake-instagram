9fe53d61791e348f1d5145de654afb3210387924
renatokano <renatokano16@gmail.com>
2020-04-30 20:06:10 -0300

    Add: express-session configuration

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require('express-session');
 
 var indexRouter = require("./routes/index");
 
@@ -15,6 +16,13 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "HJn2pWEiKbemzxLoEy4GfzqEvubdMPpn9RbvDYKHPtw1iybfNkmVYf7rYoozEU",
+    resave: true,
+    saveUninitialized: true
+  })
+)
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
