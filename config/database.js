c626852efe438f229941cf6f303d96e14b6d6237
renatokano <renatokano16@gmail.com>
2020-04-30 14:59:12 -0300

    Change: sequelize db config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,11 @@
-module.exports = {
-  username: 'root',
-  password: '1234',
+// need to be updated w/ database informations
+
+let config = {
+  username: 'root', 
+  password: 'docker',
   database: 'fake-instagram',
   host: '127.0.0.1',
   dialect: 'mysql'
 }
+
+module.exports = config;
