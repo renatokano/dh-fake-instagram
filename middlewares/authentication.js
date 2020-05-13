35f6383add4fd77deed8dd2b527b110a93eca0ae
renatokano <renatokano16@gmail.com>
2020-05-13 19:07:29 -0300

    Add: authentication middleware

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,6 +1,8 @@
 module.exports = (req, res, next) => {
   // user not logged in
   if(!req.session.user) res.redirect('/login');
+
+  res.locals.user = req.session.user;
   // user has a session
   next();
 }
