a27a3d2f9058ca4a05ad2041768c9e8af7cfe49c
renatokano <renatokano16@gmail.com>
2020-05-13 19:10:01 -0300

    Add: Publication and Comment routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const usersController = require('../controllers/usersController');
 const authenticationMiddleware = require('../middlewares/authentication');
+const publicationsController = require('../controllers/publicationsController');
+const commentsController = require('../controllers/commentsController');
+
+const upload = require('../config/uploads');
 
 // sign up
 router.get("/registro", usersController.create);
@@ -12,6 +16,11 @@ router.get("/", authController.create);
 router.get("/login", authController.create);
 router.post("/login", authController.store);
 // user page
-router.get("/home", authenticationMiddleware, usersController.home);
-
+router.get("/home", authenticationMiddleware, publicationsController.index);
+// publication page
+router.get('/publicar', authenticationMiddleware, publicationsController.create);
+router.post('/publicar', upload.any(), publicationsController.store);
+router.get('/curtir/:id', authenticationMiddleware, publicationsController.update);
+// comments
+router.post('/comentar', authenticationMiddleware, commentsController.store);
 module.exports = router;
